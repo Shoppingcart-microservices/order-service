a3e04bef81a43662276ee26b833b55e2375c4e65
Konstantinos Paidarakis <kpaidarakis.external@epo.org>
2025-07-15 19:23:40 +0300

    add controllerAdvice for handling exceptions in Order service.

---
+++ src/main/java/com/microservices/orderservice/exception/GlobanOrderServiceExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.microservices.orderservice.exception;
+
+import com.microservices.orderservice.model.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GlobanOrderServiceExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(OrderServiceCustomException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ResponseBody
+    public ResponseEntity<ErrorResponse> handleOrderServiceException(OrderServiceCustomException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse(e.getMessage(), e.getErrorCode(), HttpStatus.NOT_FOUND));
+    }
+}
--- src/main/java/com/microservices/orderservice/model/ErrorResponse.java
+++ src/main/java/com/microservices/orderservice/model/ErrorResponse.java
@@ -1,9 +1,13 @@
 package com.microservices.orderservice.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ErrorResponse {
     private String errorMessage;
     private int errorCode;
