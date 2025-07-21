a3db102a9b7810daa94ba871615b0bf731cd887c
Konstantinos Paidarakis <kpaidarakis.external@epo.org>
2025-07-21 23:39:00 +0300

    add webSecutiry filter and interceptors on Order service and removed RestTemplate.

--- src/main/java/com/microservices/orderservice/OrderServiceApplication.java
+++ src/main/java/com/microservices/orderservice/OrderServiceApplication.java
@@ -2,10 +2,7 @@ package com.microservices.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -14,11 +11,4 @@ public class OrderServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderServiceApplication.class, args);
     }
-
-    @Bean
-    @LoadBalanced
-    public RestTemplate getRestTemplate() {
-        return new RestTemplate();
-    }
-
 }
---
+++ src/main/java/com/microservices/orderservice/external/interceptors/OAuthRequestInterceptor.java
@@ -0,0 +1,32 @@
+package com.microservices.orderservice.external.interceptors;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+
+@Configuration
+public class OAuthRequestInterceptor implements RequestInterceptor {
+
+    private OAuth2AuthorizedClientManager oAuth2AuthorizedClientManager;
+
+    public OAuthRequestInterceptor(OAuth2AuthorizedClientManager oAuth2AuthorizedClientManager) {
+        this.oAuth2AuthorizedClientManager = oAuth2AuthorizedClientManager;
+    }
+
+    @Override
+    public void apply(RequestTemplate template) {
+        template.header("Name", "Konstantinos");
+        template.header("Accept", "application/json");
+        template.header("Content-Type", "application/json");
+        template.header("Authorization", "Bearer " + getToken());
+    }
+
+    private String getToken() {
+        return oAuth2AuthorizedClientManager.authorize(OAuth2AuthorizeRequest
+                .withClientRegistrationId("internal-client")
+                .principal("internal")
+                .build()).getAccessToken().getTokenValue();
+    }
+}
