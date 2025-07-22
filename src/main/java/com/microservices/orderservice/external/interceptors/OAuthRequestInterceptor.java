928a12fd108b7d23bc6d297791d4564f0ed5e049
Konstantinos Paidarakis <kpaidarakis.external@epo.org>
2025-07-22 20:11:58 +0300

    correction of some scope names

--- src/main/java/com/microservices/orderservice/external/interceptors/OAuthRequestInterceptor.java
+++ src/main/java/com/microservices/orderservice/external/interceptors/OAuthRequestInterceptor.java
@@ -3,7 +3,11 @@ package com.microservices.orderservice.external.interceptors;
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
 
 @Configuration
@@ -21,9 +25,13 @@ public class OAuthRequestInterceptor implements RequestInterceptor {
     }
 
     private String getToken() {
-        return oAuth2AuthorizedClientManager.authorize(OAuth2AuthorizeRequest
+        Authentication principal = new AnonymousAuthenticationToken(
+                "internal", "internal", AuthorityUtils.createAuthorityList("ROLE_SYSTEM"));
+        OAuth2AuthorizedClient client = oAuth2AuthorizedClientManager.authorize(OAuth2AuthorizeRequest
                 .withClientRegistrationId("internal-client")
-                .principal("internal")
-                .build()).getAccessToken().getTokenValue();
+                .principal(principal)
+                .build());
+        String tokenValue = client.getAccessToken().getTokenValue();
+        return tokenValue;
     }
 }
